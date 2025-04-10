c29c25558e7a48f52f6f4fc61b63dc4f5b1c8d74
romain22222 <romrom22222@hotmail.fr>
2025-04-10 15:31:52 +0200

    fix job path

--- job.sh
+++ job.sh
@@ -8,5 +8,5 @@
 #SBATCH --mem=60GB
 #SBATCH --time=00:10:00
 
-
+echo $CUDA_VISIBLE_DEVICES
 /home/negror/VisibilityLattices/build/visibilityLattices -i /home/negror/VolGallery/Stanford-bunny/bunny-64.vol --noInterface -r 20
