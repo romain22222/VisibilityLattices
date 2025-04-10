71283db932caa85275f753df3bd723bc2be7d96c
romain22222 <romrom22222@hotmail.fr>
2025-04-10 16:03:44 +0200

    bigger job

--- job.sh
+++ job.sh
@@ -3,15 +3,15 @@
 #SBATCH --partition=dayCPU
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=64
+#SBATCH --cpus-per-task=128
 ## SBATCH --gres=gpu:1
-#SBATCH --mem=60GB
+#SBATCH --mem=200GB
 #SBATCH --time=00:10:00
 
 # export CUDA_VISIBLE_DEVICES=0
 # echo $CUDA_VISIBLE_DEVICES
 
-export OMP_NUM_THREADS=64
+export OMP_NUM_THREADS=128
 echo $OMP_NUM_THREADS
 
 /home/negror/VisibilityLattices/build/visibilityLattices -i /home/negror/VolGallery/Stanford-bunny/bunny-1024.vol --noInterface -r 60
