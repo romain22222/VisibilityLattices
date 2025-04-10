bd0cc04247aec744a273d8c2d4186f91df9e1921
romain22222 <romrom22222@hotmail.fr>
2025-04-10 15:52:21 +0200

    check cpu

--- job.sh
+++ job.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 #SBATCH --job-name=visibilityTest
-#SBATCH --partition=dayGPU
+#SBATCH --partition=dayCPU
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=64
+## SBATCH --gres=gpu:1
 #SBATCH --mem=60GB
 #SBATCH --time=00:10:00
 
-export CUDA_VISIBLE_DEVICES=0
-echo $CUDA_VISIBLE_DEVICES
+# export CUDA_VISIBLE_DEVICES=0
+# echo $CUDA_VISIBLE_DEVICES
+
+
 /home/negror/VisibilityLattices/build/visibilityLattices -i /home/negror/VolGallery/Stanford-bunny/bunny-64.vol --noInterface -r 20
