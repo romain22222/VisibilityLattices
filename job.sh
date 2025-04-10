5975fb0806359170fdd8994e9b99fe114cc4d182
romain22222 <romrom22222@hotmail.fr>
2025-04-10 15:25:25 +0200

    job machine multithread

---
+++ job.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=visibilityTest
+#SBATCH --partition=dayGPU
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:1
+#SBATCH --mem=60GB
+#SBATCH --time=00:10:00
+
+
+./visibilityLattices -i ../../VolGallery/Stanford-bunny/bunny-64.vol --noInterface -r 20
